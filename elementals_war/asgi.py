feeecad2f693959d2cc7520797ce3fa28512418e
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-03-03 13:51:49 +0100

    adding a rest of files in a new Django project

---
+++ elementals_war/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for elementals_war project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elementals_war.settings')
+
+application = get_asgi_application()
