5368074d13d8b7884b57ab271b4c0d4d7c5188db
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-03-11 08:28:18 +0100

    Adding str methods in models. Removing models which are not used.

--- elementals_app/models.py
+++ elementals_app/models.py
@@ -3,50 +3,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from django.utils import timezone
 
 
-class ElementTile(models.Model):
-    element_type = models.CharField(max_length=40)
-    face_up = models.BooleanField(default=False)
-    image = models.ImageField(upload_to='element_images/', blank=True, null=True)
-
-    def __str__(self):
-        if self.image:
-            return f"{self.element_type} ({self.image.url})"
-        else:
-            return self.element_type
-
-
-class Wizard(models.Model):
-    wizard_type = models.CharField(max_length=40)
-    collected = models.BooleanField(default=False)
-    image = models.ImageField(upload_to='wizard_images/', blank=True, null=True)
-
-    def __str__(self):
-        if self.image:
-            return f"{self.wizard_type} ({self.image.url})"
-        else:
-            return self.wizard_type
-
-
-class Player(models.Model):
-    name = models.CharField(max_length=50)
-    wizards = models.ManyToManyField(Wizard)
-
-
-class Board(models.Model):
-    element_tiles = models.ManyToManyField(ElementTile)
-
-
-class Game(models.Model):
-    players = models.ManyToManyField(Player, related_name='games_played')
-    wizards = models.ManyToManyField(Wizard)
-    elements = models.ManyToManyField(ElementTile)
-    winner = models.ForeignKey(Player, related_name='games_won', on_delete=models.CASCADE, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'Game {self.id}'
-
-
 class Element(models.Model):
     ELEMENT_TYPES = (
         ('Earth', 'Earth'),
@@ -59,6 +15,9 @@ class Element(models.Model):
     element_type = models.CharField(max_length=20, choices=ELEMENT_TYPES)
     image = models.ImageField(storage=S3Boto3Storage())
     uploaded_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.element_type} element."
 
 
 class Entity(models.Model):
@@ -74,3 +33,5 @@ class Entity(models.Model):
     image = models.ImageField(storage=S3Boto3Storage())
     uploaded_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return f"{self.entity_type} entity."
--- elementals_war/settings.py
+++ elementals_war/settings.py
@@ -122,8 +122,6 @@ USE_TZ = True
 USE_S3 = os.getenv('USE_S3') == "TRUE"
 
 if USE_S3:
-    print("We're using AWS")
-
     #  AWS Settings
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
